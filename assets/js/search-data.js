686eea3e0597cb08a3b5409b696a8ad47cc49efc
jmtsuji <mtsuji@wlu.ca>
2026-09-03 21:02:18 +0000

    Deploying to gh-pages from @ jmtsuji/jmtsuji.github.io@20df56cbcd44a69184cc50eb4b36fe776abe5d6b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -1,7 +1,7 @@
 
 const currentUrl = window.location.href;
-const siteUrl = "https://jmtsuji.github.io";
-let updatedUrl = currentUrl.replace("https://jmtsuji.github.io", "");
+const siteUrl = "https://microbiogeo.ca";
+let updatedUrl = currentUrl.replace("https://microbiogeo.ca", "");
 if (currentUrl.length == updatedUrl.length && currentUrl.startsWith("http://127.0.0.1")) {
   const otherSiteUrl = siteUrl.replace("localhost", "127.0.0.1");
   updatedUrl = currentUrl.replace(otherSiteUrl + "", "");
